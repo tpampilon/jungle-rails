596d474ac0afa36d6d0b8e0434f2c4d29a3288ea
TedP <ted.pampilon@gmail.com>
2021-04-29 06:07:43 +0000

    add fix to About Us nav and add categories in dropdown menu in Admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,31 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
 
   def index
     @categories = Category.order(id: :desc).all
   end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(categories_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def categories_params
+
+    params.permit(:name)
   
+  end
+
+
 end
