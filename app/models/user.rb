629d995a0e212536b7087a820432e1c673dadaff
TedP <ted.pampilon@gmail.com>
2021-05-03 23:52:27 +0000

    add user spec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,18 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password)
+       return @user
+    end
+    nil
+  end
+
 end
